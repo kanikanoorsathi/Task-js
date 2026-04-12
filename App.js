914f6fa05ed62c62c544e2b85c473985a0965eb4
kanikanoorsathi <kahinurakter504@gmail.com>
2026-04-12 23:48:50 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+ const readline = require("readline");
+
+ const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+ });
+
+ rl.question("Enter your name :" , (name)=>{
+    if (!name.trim()){
+        console.log("Name cannot empty");
+        rl.close();
+        return;
+    }
+    rl.question("Enter your age:", (age) =>{
+
+        const numAge = Number (age);
+        
+        if (isNaN(numAge)){
+            console.log("Age must be a number");
+
+
+        } else if (numAge < 0){
+            console.log("Age cannot be negative");
+
+        } else {
+            console .log (`Hello, ${name}!` );
+            if(numAge >= 18){
+                console.log("Your are an Adult");
+            } else {
+                console.log("You are a Minor") ;
+            }
+        }
+       rl.close();
+    })
+    
+ })
