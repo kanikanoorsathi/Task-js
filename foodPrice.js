07a8ff52f66d55d1bcbeb4b0a5fca256804abe55
kanikanoorsathi <kahinurakter504@gmail.com>
2026-04-20 23:59:51 +0600

    Modulud Operator added

--- foodPrice.js
+++ foodPrice.js
@@ -5,5 +5,32 @@ var applePrice = 200;
 var totalPrice =orangePrice + applePrice;
 var returnMoney = 1000 - totalPrice;
 
-console .log(returnMoney);
+// console.log(orangePrice, applePrice, returnMoney);
 
+// student marks
+
+var Mathematics = 75;
+var Biology = 25;
+var Chemistry = 65;
+var Physics = 80;
+var Bangla = 90;
+var totlaMarks = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+var result = totlaMarks / 5;
+
+// console.log("Total:", totlaMarks);
+// console.log("Average:", result);
+
+// Modulud Operator
+
+var num1 = 5;
+var num2 =2;
+var result = num1 % num2;
+console.log(result);
+
+var num3 = 324;
+var num4 = 5;
+var totalresult = num3 / num4;
+var result = num3 % num4;
+
+console.log(totalresult, result);
